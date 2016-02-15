a5644a4c5ce5a117c6125eca259d224441b6d91a
woodychang0611 <woodychang0611@gmail.com>
2016-02-15 17:17:52 +0800

    2016/2/15

--- SketchBoard/Scripts/SketchCreate.js
+++ SketchBoard/Scripts/SketchCreate.js
@@ -1,8 +1,10 @@
-﻿var myApp = angular.module("myApp", []);
+﻿//AngularJS
+var myApp = angular.module("myApp", []);
 myApp.controller("myController",
     function ($scope) {
         $scope.password = "";
         $scope.repeatPassword = "";
+        $scope.data = "";
         $scope.isPasswordRepeat = true;
 
         $scope.$watchCollection('[password,repeatPassword]',
@@ -17,3 +19,107 @@ myApp.controller("myController",
             }
         )
     });
+
+//Canvas Drawing
+
+function getXY(event) {
+    var x = 0;
+    var y = 0;
+    var offsetX = canvas.getBoundingClientRect().left;
+    var offsetY = canvas.getBoundingClientRect().top;
+    if (event == '[object MouseEvent]') {
+        x = event.clientX - offsetX;
+        y = event.clientY - offsetY;
+    }
+    else if (event == '[object TouchEvent]')
+    {
+        var p =event.changedTouches[0]
+        x = p.clientX - offsetX;
+        y = p.clientY - offsetY;
+    }
+    var ratio =$('#main')[0].width / parseInt($('#main').css('width')); 
+    x = parseInt(x * ratio);
+    y = parseInt(y * ratio);
+    var pos = { x: x, y: y };
+    return pos;
+}
+
+function distance(pos1, pos2) {
+    var detX = (pos1.x - pos2.x);
+    var detY = (pos1.y - pos2.y);
+    return Math.sqrt(detX * detX + detY * detY);
+}
+
+function startDrawing(event) {
+    count = 0;
+    var pos = getXY(event);
+    context.beginPath();
+    context.moveTo(pos.x, pos.y);
+    currentColor= colors[colorIndex];
+    context.strokeStyle = currentColor;
+    context.lineWidth = 5;
+    $('#output').html("mouseDownReporter");
+    colorIndex = (colorIndex + 1) % colors.length;
+    isDrawing = true;
+    xPoints = [pos.x];
+    yPoints = [pos.y];
+
+}
+function endDrawing() {
+
+    if (isDrawing) {
+        var temp = { color: currentColor, xPoints: xPoints, yPoints: yPoints };
+        data.push(temp);
+
+        var str = JSON.stringify(data);
+        //var str = data.toString();
+        $('#output').html(str);
+        isDrawing = false;
+    }
+}
+
+
+
+function moveReporter(event) {
+    if (isDrawing) {
+        var pos = getXY(event);
+        var str = "";
+        str = "x:" + pos.x + ",y:" + pos.y + ",count:" + count;
+        $('#output').html(str);
+        if (distance(pos, lastPos) > 10) {
+            count++;
+            context.lineTo(pos.x, pos.y);
+            xPoints.push(pos.x);
+            yPoints.push(pos.y);
+
+            //  context.quadraticCurveTo(lastPos.x , lastPos.y, pos.x, pos.y);
+            context.stroke();
+            lastPos = pos;
+            document.body.style.overflowY = "hidden";
+        }
+    }
+    return true;
+}
+
+var canvas = null;
+var context = null;
+var colors = ['red', 'blue', 'lime', 'yellow', 'cyan','orange'];
+var colorIndex = 0;
+var isDrawing = false;
+var currentColor = '';
+var lastPos = { x: 0, y: 0 };
+var xPoints = [];
+var yPoints = [];
+var data = [];
+
+$(function () {
+    canvas = $('#main')[0];
+    context = canvas.getContext('2d');
+    canvas.addEventListener('mousedown', startDrawing);
+    canvas.addEventListener('mouseup', endDrawing);
+    canvas.addEventListener('mousemove', moveReporter);
+    canvas.addEventListener('mouseout', endDrawing);
+    canvas.addEventListener('touchstart', startDrawing);
+    canvas.addEventListener('touchmove', moveReporter);
+    canvas.addEventListener('touchend', endDrawing);
+})
---
+++ SketchBoard/Scripts/sketchBoard.js
@@ -0,0 +1,40 @@
+﻿function redrawCanvas(targetClass) {
+    $('.' + targetClass).each(
+        function () {
+            //Test purpose only
+            dummyDrawCanvas($(this)[0], $(this).attr('draw-data'));
+        }
+     )
+
+}
+
+function resizeCanvas(targetClass) {
+
+    $('.' + targetClass).each(
+        function () {
+            var w = $(this).parent().width();
+            $(this).css('width', w);
+            $(this).css('height', w * 618/1000);
+        }
+     )
+}
+
+//Test purpose only
+function dummyDrawCanvas(target, data) {
+    var canvas = target;
+    var ctx = canvas.getContext("2d");
+
+    ctx.lineWidth = 15;
+    ctx.strokeStyle = 'yellow';
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(500, 500);
+    ctx.stroke();
+    ctx.moveTo(500, 0);
+    ctx.lineTo(0, 500);
+    ctx.stroke();
+
+    ctx.font = "50px Arial";
+    ctx.fillText(data, 500, 500);
+
+}
